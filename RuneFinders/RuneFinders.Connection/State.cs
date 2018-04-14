adcbc4f2eecc1e5f97adef380f95755d39f59916
avlo2000 <32394484+avlo2000@users.noreply.github.com>
2018-04-14 11:43:09 +0300

    Added connect assembly

---
+++ RuneFinders/RuneFinders.Connection/State.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuneFinders.Connection
+{
+    class State : IState
+    {
+        int[,] hex_matrix;
+        Hero[] heroes;
+        public void ChangeState(IState newState)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
